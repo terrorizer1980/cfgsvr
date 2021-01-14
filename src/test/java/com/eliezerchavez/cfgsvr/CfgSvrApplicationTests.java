00f3b3316f0a5c926f8e489beed2616093632a6a
Eliezer Efrain Chavez <eliezer.chavez@gmail.com>
2021-01-14 12:02:08 -0500

    added first test

--- src/test/java/com/eliezerchavez/cfgsvr/CfgSvrApplicationTests.java
+++ src/test/java/com/eliezerchavez/cfgsvr/CfgSvrApplicationTests.java
@@ -1,13 +1,34 @@
 package com.eliezerchavez.cfgsvr;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.ResourceAccessException;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class CfgSvrApplicationTests {
 
+	@LocalServerPort
+	int randomServerPort;
+
+	@Autowired
+  	private TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void testBadCertificate() throws Exception {
+		assertThrows(ResourceAccessException.class, () -> {
+			restTemplate.exchange("/http/dev", HttpMethod.GET, null, String.class);
+		});
+		
 	}
 
 }
